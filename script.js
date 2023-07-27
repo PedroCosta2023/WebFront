be7b78df34806d3bf31a1754b7d1bcb928062c79
PedroCosta2023 <lucaspedrolucio@gmail.com>
2023-07-27 19:47:39 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,16 @@
+var nome = "Pedro";
+let idade = "21";
+const anoNascimento = 2001;
+let idadeMina = "15";
+
+console.log(idade === idadeEsposa);
+
+let listaCompras = ["Maça", "Carro", 1, 2, 3, 4]
+
+console.log(listaCompras);
+
+listaCompras.push("Moto");
+
+console.log(listaCompras.length);
+
+console.log(listaCompras[3]);
