d7533bbbac5258ed5e38048196b340dbc9c8ea44
PedroCosta2023 <lucaspedrolucio@gmail.com>
2023-07-27 21:23:48 -0300

    Desafio atualização

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const nomeInput = document.getElementById("nome")
 const email = document.getElementById("email")
 const tell = document.getElementById("tell")
 const resultado = document.getElementById("resultado")
+const resultado1 = document.getElementById("resultado1")
+const resultado2 = document.getElementById("resultado2")
 
 console.log(titulo.textContent)
 
@@ -11,15 +13,7 @@ titulo.textContent = "Senai Cotia 2023"
 function salvar() {
 
     resultado.textContent = nomeInput.value
-    //resultado1.textContent = email.value
-    //resultado2.textContent = telsl.value
-
-}
-
-function salvar1() {
     resultado1.textContent = email.value
-}
-
-function salvar2() {
     resultado2.textContent = tell.value
+
 }
